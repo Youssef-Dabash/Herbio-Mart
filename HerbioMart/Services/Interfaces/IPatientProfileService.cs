64b227b5087f1a6d0c2e72a53f004104b79ac6b8
rawdaislam <rawdaislam9999@gmail.com>
2026-09-13 01:31:07 +0300

    Add files via upload

---
+++ HerbioMart/Services/Interfaces/IPatientProfileService.cs
@@ -0,0 +1,12 @@
+﻿using HerbioMart.ViewModels.PatientProfile;
+using System.Threading.Tasks;
+
+namespace HerbioMart.Services.Interfaces
+{
+    public interface IPatientProfileService
+    {
+        Task<PatientProfileVM?> GetProfileAsync(int patientId);
+        Task<bool> UpdateProfileAsync(EditPatientProfileVM model, int patientId);
+        Task<bool> DeleteAccountAsync(int patientId); 
+    }
+}
